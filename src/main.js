a0bf8e3349992b7379277aebfdde1b03873c802a
Kat Hebert-Schmidt <kat.hebert-schmidt@impact.com>
2025-03-13 16:59:38 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+//import * as squatch from "@saasquatch/squatch-js";
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://docs.saasquatch.com/developer/squatchjs" target="_blank">
+      <img src="assets/saasquatch-icon.png" class="logo saasquatch" alt="SaaSquatch logo">
+    </a>
+    
+    <h1>SaaSquatch Onboarding</h1>
+    <nav>
+      <a href="index.html">Home</a> • 
+      <a href="widget.html">Widget</a> • 
+      <a href="landing.html">Landing</a> • 
+      <a href="event.html">Event</a>
+    </nav>
+
+  </div>
+`
